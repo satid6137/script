f8bcd70f15cc2cce577b7d2889a9d2be03d6d2cf
Satid@ITBaby <64000023+satid6137@users.noreply.github.com>
2026-02-25 10:11:34 +0700

    Update database connection settings in config.php
    แก้ไข หน้า index บางบรรทัดแสดงเป็นภาษาต่างดาว

--- config.php
+++ config.php
@@ -8,26 +8,28 @@ session_name("script");
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
-$host = 'localhost';
+//script
+$host = 'xxx'; //ตัวอย่าง 127.0.0.1
 $db   = 'xxx';
 $user = 'xxx';
 $pass = 'xxx';
 
-// ค่าคงที่เพิ่มเติม nodjs-server
-$ipServer = 'xxx';
-$hosCode  = 'xxxxxx';
-$apiKey   = 'qqq@qqq'; // 💡 ใส่ API key ไว้ตรงนี้ได้เลย
-$summaryKey = 'aaa@aaa'; // 🔐 key สำหรับ summary เท่านั้น
-$nodejs = 'http://eee:3000';
-$delete = 'http://eee:3000';
-
-// เชื่อมต่อฐานข้อมูล
 $conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// ⭐ ต้องอยู่ตรงนี้ และต้องมีครั้งเดียว
+$conn->set_charset("utf8mb4");
+
+// nodjs-server
+$ipServer = 'xxx';
+$hosCode  = 'xxxxxx';
+$apiKey   = 'qqq@qqq'; // 💡 ใส่ API key ไว้ตรงนี้ได้เลย
+$summaryKey = 'aaa@aaa'; // 🔐 key สำหรับ summary เท่านั้น
+$nodejs = 'http://eee:3000'; //IP Public
+$delete = 'http://eee:3000'; //ip nodejs-server
 
 ?>
 
